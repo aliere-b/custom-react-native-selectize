dd06cb6cc50cbd005d1c4dbd2b28fcefbaff7198
Abalo Alière Beketi <abi@edifixio.com>
2020-08-11 14:56:12 +0200

    First commit: init

---
+++ README.md
@@ -0,0 +1,4 @@
+# A React Native component inspired to Selectize which follows Material Design guidelines.
+
+
+react-native-material-selectize is [MIT licensed](LICENSE.md).
---
+++ index.js
@@ -0,0 +1,5 @@
+import Chip from './src/react-native-chip';
+import Selectize from './src/react-native-selectize';
+import UniqueSelectize from './src/react-native-unique-selectize'
+
+export { Chip, Selectize, UniqueSelectize };
